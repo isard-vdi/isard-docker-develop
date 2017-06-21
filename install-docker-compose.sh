c9972c538d244556eaa03eab2cecb8b911c37345
darta <jvinolas@correu.escoladeltreball.org>
2017-06-22 00:46:59 +0200

    docker-compose install script

--- install-docker-compose.sh
+++ install-docker-compose.sh
@@ -1,13 +1,13 @@
-sudo dnf remove docker \
+sudo dnf -y remove docker \
                   docker-common \
                   container-selinux \
                   docker-selinux \
                   docker-engine
 sudo dnf -y install dnf-plugins-core
-sudo dnf config-manager \
+sudo dnf -y config-manager \
     --add-repo \
     https://download.docker.com/linux/fedora/docker-ce.repo
-sudo dnf makecache fast
-sudo dnf install docker-ce docker-compose -y
+sudo dnf -y makecache fast
+sudo dnf -y install docker-ce docker-compose -y
 sudo systemctl enable docker
 sudo systemctl start docker
