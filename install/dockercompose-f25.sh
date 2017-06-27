000c6e74de289d3a9e1c73f9745e6d15653aa52c
root <root@backup-isard.escoladeltreball.org>
2017-06-27 16:41:13 +0200

    Changed folder structure

---
+++ install/dockercompose-f25.sh
@@ -0,0 +1,13 @@
+sudo dnf -y remove docker \
+                  docker-common \
+                  container-selinux \
+                  docker-selinux \
+                  docker-engine
+sudo dnf -y install dnf-plugins-core
+sudo dnf -y config-manager \
+    --add-repo \
+    https://download.docker.com/linux/fedora/docker-ce.repo
+sudo dnf -y makecache fast
+sudo dnf -y install docker-ce docker-compose -y
+sudo systemctl enable docker
+sudo systemctl start docker
