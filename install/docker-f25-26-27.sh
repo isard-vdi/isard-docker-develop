70faeccf0805aa17e7365fdc114223d90caec206
root <root@localhost.localdomain>
2018-01-20 14:42:55 +0100

    Updated docker install

--- install/docker-f25-26-27.sh
+++ install/docker-f25-26-27.sh
@@ -7,7 +7,8 @@ sudo dnf -y install dnf-plugins-core
 sudo dnf -y config-manager \
     --add-repo \
     https://download.docker.com/linux/fedora/docker-ce.repo
-sudo dnf -y makecache fast
+sudo dnf -y makecache 
+#sudo dnf config-manager --set-enabled docker-ce-test
 sudo dnf -y install docker-ce docker-compose -y
 sudo systemctl enable docker
 sudo systemctl start docker
