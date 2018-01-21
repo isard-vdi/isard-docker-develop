1b88a005b8b1521386f75d1af2450de9c6a640c1
root <root@mini.localdomain>
2018-01-22 00:37:08 +0100

    Check file to download before launching thread

--- dockers/hypervisor/downupdates.py
+++ dockers/hypervisor/downupdates.py
@@ -28,10 +28,11 @@ while not code:
 
 print('Starting download updates threads')
 class DownloadThread(threading.Thread,object):
-    def __init__(self,table,path,id):
+    def __init__(self,table,path,id,web_url=False):
         self.table=table
         self.path=path
         self.id=id
+        self.web_url=web_url
         self.conn = r.connect(host=RDB_HOST, port=RDB_PORT, db=TODO_DB)
         threading.Thread.__init__(self)
         self.stop=False
@@ -42,7 +43,10 @@ class DownloadThread(threading.Thread,object):
         try:
             os.makedirs(self.path.rsplit('/',1)[0], exist_ok=True)
             with open(self.path, "wb") as f:
-                response = requests.get(url+'/storage/'+self.table+'/'+self.path.split('/')[-1], headers={'Authorization':code}, stream=True)
+                if self.web_url:
+                    response = requests.get(self.web_url, stream=True)
+                else:
+                    response = requests.get(url+'/storage/'+self.table+'/'+self.path.split('/')[-1], headers={'Authorization':code}, stream=True)
                 total_length = response.headers.get('content-length')
                 
                 r.table(self.table).get(self.id).update({'status':'Downloading'}).run(rdb_conn)
@@ -79,7 +83,18 @@ def run():
                     if 'old_val' not in c or c['old_val'] is None:
                         # Initial status
                         path='/'+c['new_val']['table']+'/'+c['new_val']['path'] if c['new_val']['table']=='media' else '/groups/'+c['new_val']['create_dict']['hardware']['disks'][0]['file']
-                        downloadThreads[c['new_val']['id']]=DownloadThread(c['new_val']['table'],'/isard'+path,c['new_val']['id'])
+                        web_url=False
+                        if 'web-url' in c['new_val'].keys():
+                            i=0
+                            response=requests.head(c['new_val']['web-url'])
+                            while response.status_code==302 and i<5:  # it's a redirect
+                                response=requests.head(response.headers['location'])
+                                i=i+1
+                            if response.status_code is not 200:
+                                web_url=False
+                            else:
+                                web_url=response.url
+                        downloadThreads[c['new_val']['id']]=DownloadThread(c['new_val']['table'],'/isard'+path,c['new_val']['id'],web_url=web_url)
                         downloadThreads[c['new_val']['id']].daemon = True
                         downloadThreads[c['new_val']['id']].start()
                         pprint.pprint(downloadThreads)
