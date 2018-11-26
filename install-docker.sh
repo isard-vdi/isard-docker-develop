b0adc2b9d2f6ac31063f303d0cc7e020bce0e642
root <root@isard.localhost>
2018-11-26 15:43:17 -0600

    cleaning

--- install-docker.sh
+++ install-docker.sh
@@ -1,14 +1,11 @@
-sudo dnf -y remove docker \
-                  docker-common \
-                  container-selinux \
-                  docker-selinux \
-                  docker-engine
-sudo dnf -y install dnf-plugins-core
-sudo dnf -y config-manager \
-    --add-repo \
-    https://download.docker.com/linux/fedora/docker-ce.repo
-sudo dnf -y makecache 
-#sudo dnf config-manager --set-enabled docker-ce-test
-sudo dnf -y install docker-ce docker-compose -y
-sudo systemctl enable docker
-sudo systemctl start docker
+# Add docker
+apk add docker
+rc-update add docker boot
+
+# Add docker-compose
+apk add py-pip
+pip install docker-compose
+
+# cgroup needed for hypervisor
+mount -t cgroup -o none,name=systemd cgroup /sys/fs/cgroup/systemd/
+echo 'cgroup /sys/fs/cgroup/systemd/ cgroup   none,name=systemd' >> /etc/fstab
