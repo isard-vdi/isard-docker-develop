3b7bc86816cec5f7d04eaecdd792551595cfb1ab
darta <vinolasbcn@gmail.com>
2018-12-14 18:20:37 -0600

    Viewers work with tls (except vnc with client)

--- dockers/hypervisor/customlibvirtpost.sh
+++ dockers/hypervisor/customlibvirtpost.sh
@@ -1,5 +1,5 @@
 #/bin/bash
 
 chmod 666 /dev/kvm
-virsh net-define /network.xml
-virsh net-start default
+#~ virsh net-define /network.xml
+#~ virsh net-start default
--- dockers/hypervisor/start_proxy.py
+++ dockers/hypervisor/start_proxy.py
@@ -4,10 +4,13 @@ import socket
 servers={}
 procs = {}
 
-for i in range(30):
+for i in range(50):
     servers[i]=websockify.WebSocketProxy(listen_host='0.0.0.0',
                              listen_port=55900+i,
                              target_host=socket.getfqdn(),
-                             target_port=5900+i)
+                             target_port=5900+i,
+                             cert="/etc/pki/libvirt-spice/server-cert.pem",
+                             key="/etc/pki/libvirt-spice/server-key.pem")
     procs[i] = multiprocessing.Process(target=servers[i].start_server)
     procs[i].start()
+
